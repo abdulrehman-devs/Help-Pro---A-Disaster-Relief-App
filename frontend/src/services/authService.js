4f919a18a02ca267010bb8dadb006efeddc660fe
Abdul Rehman <abdulrehmanfaheem1@gmail.com>
2026-05-10 17:29:49 +0500

    URLs Changing

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API = `http://localhost:5000/api`;
 const API_URL = "https://help-pro-a-disaster-relief-app.onrender.com";
 
-export const signin = (data) =>
-  axios.post(`${API_URL}/user/signin`, data);
-
 export const signup = (data) =>
-  axios.post(`${API_URL}/user/signup`, data);
+  axios.post(`${API_URL}/api/user/signup`, data);
+
+export const signin = (data) =>
+  axios.post(`${API_URL}/api/user/signin`, data);
 
 export const adminSignin = (data) =>
-  axios.post(`${API_URL}/admin/signin`, data);
+  axios.post(`${API_URL}/api/admin/signin`, data);
